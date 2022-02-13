ba29aece427d12aab5e6bd9199fb7788be78c12d
ISILV125 <igonildo7@gmail.com>
2022-02-13 14:59:10 -0300

    Adding widgets in c++

--- Source/LightPainter/PaintingGrid.cpp
+++ Source/LightPainter/PaintingGrid.cpp
@@ -3,10 +3,17 @@
 
 #include "PaintingGrid.h"
 #include "Engine/Engine.h"
+#include "Components/SizeBox.h"
 
 void UPaintingGrid::AddPainting()
 {
-	if (PaintingGrid) {
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::Printf(TEXT("												GRID NAME: %s"), *PaintingGrid->GetName()));
-	}
+	if (!PaintingGrid) return;
+	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::Printf(TEXT("												GRID NAME: %s"), *PaintingGrid->GetName()));
+	UUserWidget* NewWidget = CreateWidget<UUserWidget>(GetWorld(), GridCardClass);
+	if (!NewWidget) return;
+
+	USizeBox* CardContrainer = Cast<USizeBox>(PaintingGrid->GetChildAt(0));
+	if (!CardContrainer) return;
+	CardContrainer->AddChild(NewWidget);
+
 }
